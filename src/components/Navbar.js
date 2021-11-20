027a525f58b1956886cf268b3c6beddd764779a5
Lordkaito <cespedesisai88@gmail.com>
2021-11-20 14:23:36 +0100

    Apply changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,17 @@ import { AiOutlineLeft } from 'react-icons/ai';
 import { BsFillGearFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import './styles/Navbar.css';
+import { useLocation } from 'react-router';
 
 const Navbar = () => {
+  const location = useLocation();
   return (
     <nav className="navbar-container">
       <div>
         <div>
           <ul className="navbar">
             <li>
-              {window.location.href === 'https://deploy-preview-1--silly-almeida-f20b56.netlify.app/#/' ? null : <Link to="/" className="home"> <AiOutlineLeft /> </Link>
+              {location.pathname !== '/' && <Link to="/" className="home"> <AiOutlineLeft /> </Link>
               }
             </li>
             <li data-testid="tab-name">
